13a9623a7a40cb05b893efae4139b880f0beebc8
Amos Folz <33728190+amosfolz@users.noreply.github.com>
2019-06-18 13:21:58 +0000

    Page 1 changes made

--- routes/pastries.php
+++ routes/pastries.php
@@ -1,9 +1,25 @@
 <?php
 
-/**
- * Routes for pastries related pages.
+/*
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @copyright Copyright (c) 2019 Alexander Weissman
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/LICENSE.md (MIT License)
  */
+
+use UserFrosting\Sprinkle\Core\Util\NoCache;
+
 $app->group('/pastries', function () {
-    $this->get('', 'UserFrosting\Sprinkle\Pastries\Controller\PastriesController:displayPage')
+    $this->get('', 'UserFrosting\Sprinkle\Pastries\Controller\PastriesController:pageList')
          ->setName('pastries');
+})->add('authGuard');
+
+// These routes will be for any methods that retrieve/modify data from the database.
+$app->group('/api/pastries', function () {
+    $this->get('', 'UserFrosting\Sprinkle\Pastries\Controller\PastriesController:getList');
+})->add('authGuard')->add(new NoCache());
+
+// These routes will be used to store any modals
+$app->group('/modals/pastries', function () {
 })->add('authGuard');
--- src/Controller/PastriesController.php
+++ src/Controller/PastriesController.php
@@ -1,13 +1,21 @@
 <?php
 
+/*
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @copyright Copyright (c) 2019 Alexander Weissman
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/LICENSE.md (MIT License)
+ */
+
 namespace UserFrosting\Sprinkle\Pastries\Controller;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use UserFrosting\Sprinkle\Core\Controller\SimpleController;
-use UserFrosting\Sprinkle\Core\Facades\Debug;
-use UserFrosting\Sprinkle\Pastries\Database\Models\Pastries;
 use UserFrosting\Support\Exception\ForbiddenException;
+use UserFrosting\Sprinkle\Pastries\Database\Models\Pastries;
+use UserFrosting\Sprinkle\Pastries\Sprunje\PastrySprunje;
 
 class PastriesController extends SimpleController
 {
@@ -31,5 +39,30 @@ class PastriesController extends SimpleController
         return $this->ci->view->render($response, 'pages/pastries.html.twig', [
             'pastries' => $pastries,
         ]);
+    }
+
+    public function getList(Request $request, Response $response, $args)
+    {
+        // GET parameters
+        $params = $request->getQueryParams();
+
+        /** @var UserFrosting\Sprinkle\Account\Authorize\AuthorizationManager */
+        $authorizer = $this->ci->authorizer;
+
+        /** @var UserFrosting\Sprinkle\Account\Database\Models\User $currentUser */
+        $currentUser = $this->ci->currentUser;
+
+        // Access-controlled page
+        if (!$authorizer->checkAccess($currentUser, 'see_pastries')) {
+            throw new ForbiddenException();
+        }
+        /** @var UserFrosting\Sprinkle\Core\Util\ClassMapper $classMapper */
+        $classMapper = $this->ci->classMapper;
+
+        $sprunje = new PastrySprunje($classMapper, $params);
+
+        // Be careful how you consume this data - it has not been escaped and contains untrusted user-supplied content.
+        // For example, if you plan to insert it into an HTML DOM, you must escape it on the client side (or use client-side templating).
+        return $sprunje->toResponse($response);
     }
 }
